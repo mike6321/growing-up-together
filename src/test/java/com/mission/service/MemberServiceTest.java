db66707cbc0b1d822c23b55c702650d7e6966ad1
bbubbush <bbubbush@gmail.com>
2022-05-12 09:41:35 +0900

    fix: Controller Naming role 적용

--- src/test/java/com/mission/service/MemberServiceTest.java
+++ src/test/java/com/mission/service/MemberServiceTest.java
@@ -1,10 +1,12 @@
 package com.mission.service;
 
 import com.mission.domain.*;
+import com.mission.dto.member.ReqCreateMember;
+import com.mission.dto.member.ReqUpdateMember;
+import com.mission.dto.member.ResFindMember;
+import com.mission.dto.member.ResModifyMember;
 import com.mission.repository.MemberRepository;
 import com.mission.repository.TopicOfInterestRepository;
-import com.mission.vo.MemberCreateVO;
-import com.mission.vo.MemberUpdateVO;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,7 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -41,7 +42,7 @@ class MemberServiceTest {
     // given
 
     // when
-    final Iterable<Member> findMembers = memberService.findAll();
+    final List<ResFindMember> findMembers = memberService.findAll();
 
     // then
     assertThat(findMembers).isNotNull();
@@ -55,7 +56,7 @@ class MemberServiceTest {
 
     // when
     when(memberRepository.findById(createMember.getId())).thenReturn(Optional.of(createMember));
-    final Member findMember = memberService.findById(createMember.getId());
+    final ResFindMember findMember = memberService.findById(createMember.getId());
 
     // then
     assertThat(findMember).isNotNull();
@@ -84,33 +85,18 @@ class MemberServiceTest {
   public void createMemberSuccess1() {
     // given
     final String expectedMemberEmail = "bbubbush99@gmail.com";
-    final MemberCreateVO createVO = createMemberOfNothingInterestVO();
+    final ReqCreateMember createVO = createMemberOfNothingInterestVO();
 
     // when
     when(memberRepository.findByEmail(expectedMemberEmail)).thenReturn(Optional.empty());
     when(memberRepository.save(any())).thenReturn(createMemberNothingInterest());
-    final Member createMember = memberService.createMember(createVO);
+    final ResModifyMember createMember = memberService.createMember(createVO);
 
     // then
     assertThat(createMember).isNotNull();
     assertThat(createMember.getId()).isNotNull()
       .isNotEqualTo(0L)
       .isGreaterThan(0L);
-    assertThat(createMember.getNickname()).isNotNull()
-      .isNotEmpty();
-    assertThat(createMember.getEmail()).isNotNull()
-      .isNotEmpty();
-    assertThat(createMember.getProfileImageUrl()).isNull();
-    assertThat(createMember.getParticipationMissions()).isNotNull();
-    assertThat(createMember.getParticipationMissions().size()).isEqualTo(0L);
-    assertThat(createMember.getTopicOfInterests()).isNotNull();
-    assertThat(createMember.getTopicOfInterests().size()).isEqualTo(0L);
-    assertThat(createMember.getGrade()).isNotNull();
-    assertThat(createMember.getGrade().getGradeStaus()).isEqualTo(GradeStaus.BEGINNER);
-    assertThat(createMember.getGrade().getPoint()).isEqualTo(0L);
-    assertThat(createMember.isTopicOfInterestAlarm()).isFalse();
-    assertThat(createMember.isEmailAuthenticate()).isFalse();
-    assertThat(createMember.isWithdrawal()).isFalse();
   }
 
   @Test
@@ -122,37 +108,18 @@ class MemberServiceTest {
       add("Spring");
       add("Java");
     }};
-    final MemberCreateVO createVO = createMemberOfInterestVO(createUserTopic);
-    final List<MemberOfTopicInterest> expectedMemberTopics = createMemberOfInterestOfTopic();
+    final ReqCreateMember createVO = createMemberOfInterestVO(createUserTopic);
 
     // when
     when(memberRepository.findByEmail(expectedMemberEmail)).thenReturn(Optional.empty());
     when(memberRepository.save(any())).thenReturn(createMemberSpringAndJavaInterest());
-    Member createMember = memberService.createMember(createVO);
+    ResModifyMember createMember = memberService.createMember(createVO);
 
     // then
     assertThat(createMember).isNotNull();
     assertThat(createMember.getId()).isNotNull()
       .isNotEqualTo(0L)
       .isGreaterThan(0L);
-    assertThat(createMember.getNickname()).isNotNull()
-      .isNotEmpty();
-    assertThat(createMember.getEmail()).isNotNull()
-      .isNotEmpty();
-    assertThat(createMember.getProfileImageUrl()).isNull();
-    assertThat(createMember.getParticipationMissions()).isNotNull();
-    assertThat(createMember.getParticipationMissions().size()).isEqualTo(0L);
-    assertThat(createMember.getTopicOfInterests()).isNotNull();
-    assertThat(createMember.getTopicOfInterests().size()).isEqualTo(2L);
-    IntStream.range(0, createMember.getTopicOfInterests().size())
-      .forEach(i -> assertThat(createMember.getTopicOfInterests().get(i).getTopicOfInterest().getName()).
-        isEqualTo(expectedMemberTopics.get(i).getTopicOfInterest().getName()));
-    assertThat(createMember.getGrade()).isNotNull();
-    assertThat(createMember.getGrade().getGradeStaus()).isEqualTo(GradeStaus.BEGINNER);
-    assertThat(createMember.getGrade().getPoint()).isEqualTo(0L);
-    assertThat(createMember.isTopicOfInterestAlarm()).isFalse();
-    assertThat(createMember.isEmailAuthenticate()).isFalse();
-    assertThat(createMember.isWithdrawal()).isFalse();
   }
 
   @Test
@@ -161,7 +128,7 @@ class MemberServiceTest {
     // given
     final String expectedMessage = "중복된 이메일 입니다.";
     final Member findMember = createMemberInDb();
-    final MemberCreateVO createVO = createMemberOfNothingInterestVO();
+    final ReqCreateMember createVO = createMemberOfNothingInterestVO();
 
     // when
     when(memberRepository.findByEmail(createVO.getEmail())).thenReturn(Optional.of(findMember));
@@ -176,34 +143,19 @@ class MemberServiceTest {
   public void updateMemberSuccess1() {
     // given
     final Member findMember = createMemberInDb();
-    final MemberUpdateVO updateVO = createUpdateMemberVO();
+    final ReqUpdateMember updateVO = createUpdateMemberVO();
 
     // when
     when(memberRepository.findByEmail(findMember.getEmail())).thenReturn(Optional.empty());
     when(memberRepository.findById(updateVO.getId())).thenReturn(Optional.of(findMember));
 
-    final Member updateMember = memberService.updateMember(updateVO);
+    final ResModifyMember updateMember = memberService.updateMember(updateVO);
 
     // then
     assertThat(updateMember).isNotNull();
     assertThat(updateMember.getId()).isNotNull()
       .isNotEqualTo(0L)
       .isGreaterThan(0L);
-    assertThat(updateMember.getNickname()).isNotNull()
-      .isNotEmpty();
-    assertThat(updateMember.getEmail()).isNotNull()
-      .isNotEmpty();
-    assertThat(updateMember.getProfileImageUrl()).isNull();
-    assertThat(updateMember.getParticipationMissions()).isNotNull();
-    assertThat(updateMember.getParticipationMissions().size()).isEqualTo(0L);
-    assertThat(updateMember.getTopicOfInterests()).isNotNull();
-    assertThat(updateMember.getTopicOfInterests().size()).isEqualTo(0L);
-    assertThat(updateMember.getGrade()).isNotNull();
-    assertThat(updateMember.getGrade().getGradeStaus()).isEqualTo(GradeStaus.BEGINNER);
-    assertThat(updateMember.getGrade().getPoint()).isEqualTo(0L);
-    assertThat(updateMember.isTopicOfInterestAlarm()).isFalse();
-    assertThat(updateMember.isEmailAuthenticate()).isFalse();
-    assertThat(updateMember.isWithdrawal()).isFalse();
   }
 
   @Test
@@ -211,7 +163,7 @@ class MemberServiceTest {
   public void updateMemberFail1() {
     // given
     final String expectedMessage = "일치하는 회원을 찾을 수 없습니다.";
-    final MemberUpdateVO updateVO = createUpdateMemberVO();
+    final ReqUpdateMember updateVO = createUpdateMemberVO();
 
     // when
     RuntimeException memberNotFoundException = assertThrows(RuntimeException.class, () -> memberService.updateMember(updateVO));
@@ -225,7 +177,7 @@ class MemberServiceTest {
   public void updateMemberFail2() {
     // given
     final String expectedMessage = "중복된 이메일 입니다.";
-    final MemberUpdateVO updateVO = createUpdateMemberVO();
+    final ReqUpdateMember updateVO = createUpdateMemberVO();
 
     // when
     when(memberRepository.findByEmail(updateVO.getEmail())).thenReturn(Optional.of(createMemberNothingInterest()));
@@ -290,11 +242,17 @@ class MemberServiceTest {
       .build();
   }
 
-  private MemberCreateVO createMemberOfInterestVO(List<String> createUserTopic) {
-    return new MemberCreateVO("bbubbush99", "bbubbush99@gmail.com", false, createUserTopic);
+  private ReqCreateMember createMemberOfInterestVO(List<String> createUserTopic) {
+    return ReqCreateMember
+      .builder()
+      .nickname("bbubbush99")
+      .email("bbubbush99@gmail.com")
+      .topicOfInterestAlarm(false)
+      .topicOfInterests(createUserTopic)
+      .build();
   }
 
-  private MemberCreateVO createMemberOfNothingInterestVO() {
+  private ReqCreateMember createMemberOfNothingInterestVO() {
     return createMemberOfInterestVO(new ArrayList<>());
   }
 
@@ -306,15 +264,22 @@ class MemberServiceTest {
       .email("bbubbush1@gmail.com")
       .participationMissions(new ArrayList<>())
       .topicOfInterests(new ArrayList<>())
-      .grade(Grade.builder().id(99L).gradeStaus(GradeStaus.BEGINNER).build())
+      .grade(createBeginnerGrade())
       .isTopicOfInterestAlarm(false)
       .isEmailAuthenticate(false)
       .isWithdrawal(false)
       .build();
   }
 
-  private MemberUpdateVO createUpdateMemberVO() {
-    return new MemberUpdateVO(99L, "bbubbush", false, null, false, false, createBeginnerGrade(), "bbubbush1@gmail.com", new ArrayList<>());
+  private ReqUpdateMember createUpdateMemberVO() {
+    return ReqUpdateMember
+      .builder()
+      .id(99L)
+      .nickname("bbubbush")
+      .email("bbubbush1@gmail.com")
+      .topicOfInterestAlarm(true)
+      .topicOfInterests(new ArrayList<>())
+      .build();
   }
 
 }
