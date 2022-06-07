3104f94b4633eb344b5faa5882ee7514162c63b2
mike6321 <mike6321@naver.com>
2022-06-07 18:48:52 +0900

    [junwoo.choi] add todo, remove exclude = SecurityAutoConfiguration.class

--- src/main/java/com/mission/controller/MissionController.java
+++ src/main/java/com/mission/controller/MissionController.java
@@ -23,13 +23,25 @@ public class MissionController {
         webDataBinder.addValidators(createMissionValidator);
     }
 
+    // TODO: 2022/06/07 미션 조회 기능 개발 (1)
+    @GetMapping("/mission")
+    public ResponseEntity<?> findMissions() {
+        return null;
+    }
+
+    // TODO: 2022/06/07 미션 조회 기능 개발 (2)
+    @GetMapping("/mission/{id}")
+    public ResponseEntity<?> findMission(@PathVariable(name = "id") Long missionId) {
+        return null;
+    }
+
     @PostMapping("/mission")
-    public ResponseEntity<?> createMission(@Valid @RequestBody ReqCreateMission reqCreateMission) {
+    public ResponseEntity<Long> createMission(@Valid @RequestBody ReqCreateMission reqCreateMission) {
         return ResponseEntity.ok(missionService.saveMission(reqCreateMission));
     }
 
     @PutMapping("/mission")
-    public ResponseEntity<?> updateMission(@Valid @RequestBody ReqUpdateMission requestUpdateMission) {
+    public ResponseEntity<Long> updateMission(@Valid @RequestBody ReqUpdateMission requestUpdateMission) {
         return ResponseEntity.ok(missionService.updateMissionInformation(requestUpdateMission));
     }
 
