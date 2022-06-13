e2102b99f1297bb4cfa85693aafe7199b5a020e4
mike6321 <mike6321@naver.com>
2022-06-13 11:19:24 +0900

    [junwoo.choi] feat : 미션 전체조회, 미션조회 적용

--- src/main/java/com/mission/controller/MissionController.java
+++ src/main/java/com/mission/controller/MissionController.java
@@ -2,6 +2,7 @@ package com.mission.controller;
 
 import com.mission.dto.mission.ReqCreateMission;
 import com.mission.dto.mission.ReqUpdateMission;
+import com.mission.dto.mission.ResFindMission;
 import com.mission.service.MissionService;
 import com.validation.CreateMissionValidator;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,21 +20,21 @@ public class MissionController {
     private final MissionService missionService;
     private final CreateMissionValidator createMissionValidator;
 
-    @InitBinder("requestCreateMission")
+    @InitBinder("reqCreateMission")
     public void initBinder(WebDataBinder webDataBinder) {
         webDataBinder.addValidators(createMissionValidator);
     }
 
-    // TODO: 2022/06/07 미션 조회 기능 개발 (1)
     @GetMapping("/mission")
-    public ResponseEntity<?> findMissions() {
-        return null;
+    public ResponseEntity<List<ResFindMission>> findMissions() {
+        List<ResFindMission> missions = missionService.getMissions();
+        return ResponseEntity.ok(missions);
     }
 
-    // TODO: 2022/06/07 미션 조회 기능 개발 (2)
     @GetMapping("/mission/{id}")
-    public ResponseEntity<?> findMission(@PathVariable(name = "id") Long missionId) {
-        return null;
+    public ResponseEntity<ResFindMission> findMission(@PathVariable(name = "id") Long missionId) {
+        ResFindMission mission = missionService.getMission(missionId);
+        return ResponseEntity.ok(mission);
     }
 
     @PostMapping("/mission")
