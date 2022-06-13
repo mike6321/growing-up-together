e65e221d735101122e0f64475c8e728e9f86eda2
mike6321 <mike6321@naver.com>
2022-06-13 21:41:10 +0900

    [junwoo.choi] fix : JwtAuthorizationFilter 오류 수정

--- src/main/java/com/mission/security/JwtAuthorizationFilter.java
+++ src/main/java/com/mission/security/JwtAuthorizationFilter.java
@@ -23,12 +23,10 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     @Override
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         SecurityContext context = SecurityContextHolder.getContext();
-        if (context.getAuthentication() != null) {
-            if (!context.getAuthentication().isAuthenticated()) {
-                Authentication authentication = getUserPasswordAuthentication(request);
-                context.setAuthentication(authentication);
-                log.info("SecurityContext에 {} 인증 정보를 저장했습니다. URI : {}", authentication.getName(), request.getRequestURI());
-            }
+        if (context.getAuthentication() == null) {
+            Authentication authentication = getUserPasswordAuthentication(request);
+            context.setAuthentication(authentication);
+            log.info("SecurityContext에 {} 인증 정보를 저장했습니다. URI : {}", authentication.getName(), request.getRequestURI());
         }
         chain.doFilter(request, response);
     }
